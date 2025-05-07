a75344c6435cf1afdd9cb0cded2a7ebcd67a9d90
TaiDev <2051063445@e.tlu.edu.vn>
2025-05-07 14:00:44 +0700

    feat: setup aurinko

---
+++ src/app/api/aurinko/callback/route.ts
@@ -0,0 +1,66 @@
+// api/aurinko/callback
+
+import { exchangeCodeForAccessToken, getAccountDetails } from "@/lib/aurinko";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const params = req.nextUrl.searchParams;
+  const status = params.get("status");
+
+  if (status !== "success") {
+    return NextResponse.json(
+      { error: "Failed to link account" },
+      { status: 400 },
+    );
+  }
+
+  // get the code to exchange for the access token
+  const code = params.get("code");
+  if (!code) {
+    return NextResponse.json({ error: "No code provided" }, { status: 400 });
+  }
+
+  const token = await exchangeCodeForAccessToken(code);
+
+  if (!token) {
+    return NextResponse.json(
+      { error: "Failed to exchange code for access token" },
+      { status: 400 },
+    );
+  }
+
+  const accountDetails = await getAccountDetails(token.accessToken);
+
+  if (!accountDetails) {
+    return NextResponse.json(
+      { error: "Failed to retrieve account details" },
+      { status: 400 },
+    );
+  }
+
+  await db.account.upsert({
+    where: {
+      id: token.accountId.toString(),
+    },
+    update: {
+      accessToken: token.accessToken,
+    },
+    create: {
+      id: token.accountId.toString(),
+      userId,
+      emailAddress: accountDetails.email,
+      name: accountDetails.name,
+      accessToken: token.accessToken,
+    },
+  });
+
+  return NextResponse.redirect(new URL("/mail", req.url));
+};
