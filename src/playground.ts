a25c250eac98121bd4eeabfd687a18248fbd5285
TaiDev <2051063445@e.tlu.edu.vn>
2025-05-12 17:54:21 +0700

    feat: search orama

--- src/playground.ts
+++ src/playground.ts
@@ -1,9 +1,37 @@
-import { Account } from "./lib/account";
+import { create, insert, search, type AnyOrama } from "@orama/orama";
+import { db } from "./server/db";
+import { OramaClient } from "./lib/orama";
 
-const acc = new Account("KFBCQr54JrbqL-IixHLLU90xCQc6_N02pvDwxeUjbSA");
-// console.log(
-//   await acc.getUpdatedEmails({
-//     deltaToken: "H4sIAAAAAAAA_2NgZmBkAAPGw4oMLWCW4AUpBpaMktwcALuhXhgdAAAA",
-//   }),
-// );
-await acc.syncEmails();
+const orama = new OramaClient("115489");
+await orama.initialize();
+
+const emails = await db.email.findMany({
+  select: {
+    subject: true,
+    body: true,
+    from: true,
+    to: true,
+    sentAt: true,
+    threadId: true,
+  },
+});
+
+for (const email of emails) {
+  // @ts-ignore
+  await orama.insert({
+    subject: email.subject,
+    body: email.body ?? undefined,
+    from: email.from.address,
+    to: email.to.map((to) => to.address),
+    sentAt: email.sentAt.toLocaleString(),
+    threadId: email.threadId,
+  });
+}
+
+const searchResult = await orama.search({
+  term: "google",
+});
+
+for (const hit of searchResult.hits) {
+  console.log(hit.document.subject);
+}
