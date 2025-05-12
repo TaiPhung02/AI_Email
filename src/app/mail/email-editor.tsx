f44833c1965295a862d9e1701a5576e0f230fcf8
TaiDev <2051063445@e.tlu.edu.vn>
2025-05-12 10:48:52 +0700

    feat: Reply Box

--- src/app/mail/email-editor.tsx
+++ src/app/mail/email-editor.tsx
@@ -6,11 +6,42 @@ import StarterKit from "@tiptap/starter-kit";
 import { Text } from "@tiptap/extension-text";
 import EditorMenuBar from "./editor-menubar";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import TagInput from "./tag-input";
+import { Input } from "@/components/ui/input";
 
-type Props = {};
+type Props = {
+  subject: string;
+  setSubject: (value: string) => void;
 
-const EmailEditor = (props: Props) => {
+  toValues: { label: string; value: string }[];
+  setToValues: (values: { label: string; value: string }[]) => void;
+
+  ccValues: { label: string; value: string }[];
+  setCcValues: (values: { label: string; value: string }[]) => void;
+
+  to: string[];
+
+  handleSend: (value: string) => void;
+  isSending: boolean;
+
+  defaultToolBarExpanded?: boolean;
+};
+
+const EmailEditor = ({
+  subject,
+  setSubject,
+  toValues,
+  setToValues,
+  ccValues,
+  setCcValues,
+  to,
+  handleSend,
+  isSending,
+  defaultToolBarExpanded,
+}: Props) => {
   const [value, setValue] = React.useState<string>("");
+  const [expanded, setExpanded] = React.useState(defaultToolBarExpanded);
 
   const CustomText = Text.extend({
     addKeyboardShortcuts() {
@@ -41,6 +72,45 @@ const EmailEditor = (props: Props) => {
         <EditorMenuBar editor={editor} />
       </div>
 
+      <div className="space-y-2 p-4 pb-0">
+        {expanded && (
+          <>
+            <TagInput
+              label="To"
+              onChange={setToValues}
+              placeHolder="Add Recipients"
+              value={toValues}
+            />
+
+            <TagInput
+              label="Cc"
+              onChange={setCcValues}
+              placeHolder="Add Recipients"
+              value={ccValues}
+            />
+
+            <Input
+              id="subject"
+              placeholder="Subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </>
+        )}
+
+        <div className="flex items-center gap-2">
+          <div
+            className="cursor-pointer"
+            onClick={() => {
+              setExpanded(!expanded);
+            }}
+          >
+            <span className="font-medium text-green-600">Draft </span>
+            <span className="font-medium">to {to.join(", ")}</span>
+          </div>
+        </div>
+      </div>
+
       <div className="prose w-full px-4">
         <EditorContent
           editor={editor}
@@ -51,7 +121,25 @@ const EmailEditor = (props: Props) => {
 
       <Separator />
 
-      <div className="flex items-center justify-between px-4 py-3"></div>
+      <div className="flex items-center justify-between px-4 py-3">
+        <span className="text-sm">
+          Tip: Press{" "}
+          <kbd className="rounded-lg border border-gray-200 bg-gray-100 px-2 py-1.5 text-xs font-semibold text-gray-800">
+            Ctrl + J
+          </kbd>{" "}
+          for AI autocomplete
+        </span>
+
+        <Button
+          onClick={async () => {
+            editor?.commands.clearContent();
+            await handleSend(value);
+          }}
+          disabled={isSending}
+        >
+          Send
+        </Button>
+      </div>
     </div>
   );
 };
